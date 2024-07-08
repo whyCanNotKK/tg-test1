3103c32c4cefe891e4f563ca4c05e5605cdbc609
kk <970811072@qq.com>
2024-07-08 18:17:05 +0800

    'raw'

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://github.com/whyCanNotKK/tg-test1/blob/main/public/tonconnect-mainfest.json';
+const manifestUrl = 'https://raw.githubusercontent.com/whyCanNotKK/tg-test1/main/public/tonconnect-mainfest.json';
 
 ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
